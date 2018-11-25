d4d1799cfaf9777a69de520ade1fdc35d9129cd4
Tatsuya Ishibashi <e185714@ie.u-ryukyu.ac.jp>
2018-11-25 23:22:27 +0900

    1st commit

---
+++ src/jp/ac/uryukyu/e185714/Exception.java
@@ -0,0 +1,8 @@
+package jp.ac.uryukyu.e185714;
+
+public class Exception {
+    public static void main(String[] args){
+        String str = null;
+        System.out.println(str.length());
+    }
+}
