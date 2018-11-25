ddbda5fcdc5262144849240a5f34d86f00ccdf2d
Tatsuya Ishibashi <e185714@ie.u-ryukyu.ac.jp>
2018-11-26 01:25:16 +0900

    2nd commit

--- src/jp/ac/uryukyu/e185714/Exception.java
+++ src/jp/ac/uryukyu/e185714/Exception.java
@@ -2,7 +2,13 @@ package jp.ac.uryukyu.e185714;
 
 public class Exception {
     public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (NullPointerException e) {
+            //NullPointerExceptionが起きたときの処理
+            System.out.println("NullPointerExceptionが発生しました。");
+            System.out.println(e.getMessage());
+        }
     }
 }
